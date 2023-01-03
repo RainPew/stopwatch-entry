6c3febe85661aa8379ca12be7c4084361cee4d8b
RainPew <hoanghuunhanh.rain@gmail.com>
2023-01-03 17:31:30 +0700

    lap time

--- project_watch/src/components/Controls/Controls.tsx
+++ project_watch/src/components/Controls/Controls.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Controls.css';
 
 type Props = {
-    setTimeInSeconds: Function
+    setTimeInSeconds: Function,
+    handleLapTime: any,
 }
 
 function Controls(props: Props) {
-    const { setTimeInSeconds } = props;
+    const { setTimeInSeconds, handleLapTime } = props;
     const [intervalId, setIntervalId] = useState<number>(0);
 
     const handlePlayButton = () => {
         const interval: any = setInterval(() => {
             setTimeInSeconds((previousState: number) => previousState + 1);
-        }, 1000);
+        }, 10);
         setIntervalId(interval);
 
     }
@@ -24,12 +25,14 @@ function Controls(props: Props) {
         clearInterval(intervalId);
         setTimeInSeconds(0);
     }
+  
 
     return (
         <div className="stopwatch-controls-container">
             <button onClick={handlePlayButton} type="button">Play</button>
             <button onClick={handleStopButton} type="button">Stop</button>
             <button onClick={handleReset} type="button">Reset</button>
+            <button onClick={handleLapTime} type="button">ShowLapTime</button>
         </div>
     );
 }
--- project_watch/src/components/main/Main.tsx
+++ project_watch/src/components/main/Main.tsx
@@ -4,13 +4,18 @@ import Controls from '../Controls/Controls';
 
 function Main() {
     const [timeInSeconds, setTimeInSeconds] = useState(0);
-    const [timeArray, setTimeArray] = useState<Array<number|string>>([]);
+    const [timeArray, setTimeArray] = useState<Array<number | string>>([]);
+    const [laps, setLaps] = useState<(any)[]>([]);
 
 
     useEffect(() => {
         setTimeArray(calculateTimeInSeconds(timeInSeconds));
     }, [timeInSeconds]);
 
+    const handleLapTime = () => {
+        const currLaps = [...laps, calculateTimeInSeconds(timeInSeconds)];
+        setLaps(currLaps)
+    }
     return (
         <main className="stopwatch-container">
             <section className="timer-display">
@@ -20,7 +25,14 @@ function Main() {
                 <span>:</span>
                 <p>{timeArray[2]}</p>
             </section>
-            <Controls setTimeInSeconds={setTimeInSeconds} />
+            <Controls setTimeInSeconds={setTimeInSeconds} handleLapTime={handleLapTime} />
+            <div>
+                {laps.map((lap, index) =>
+                    <div className="lap-display">
+                        <p>{lap[0]}:{lap[1]}:{lap[2]}</p>
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
